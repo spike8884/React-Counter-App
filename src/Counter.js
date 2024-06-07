daefda57249af711174f0a997d2892e85c252eb9
Richard <spike8884@gmail.com>
2024-06-07 13:46:27 -0400

    push

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Zero Out</button>
+    </div>
+  );
+}
+
+export default Counter;
